3a45ddf2f0be23ea6600a77a7ac1e28c90598930
“liunxsugyli” <3101967255@qq.com>
2018-04-06 12:04:33 +0800

    4.6.7

--- app/Admin/routes.php
+++ app/Admin/routes.php
@@ -8,6 +8,7 @@ Route::group([
     'prefix'        => config('admin.route.prefix'),
     'namespace'     => config('admin.route.namespace'),
     'middleware'    => config('admin.route.middleware'),
+    'domain' => config('app.admin_url')
 ], function (Router $router) {
 
     $router->get('/', 'HomeController@index');
