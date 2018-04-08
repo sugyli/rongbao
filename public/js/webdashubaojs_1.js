754bd62a44a82ee255f72e18c39d5092e57ac6ee
“liunxsugyli” <3101967255@qq.com>
2018-04-08 11:29:31 +0800

    4.8.1

--- public/js/webdashubaojs_1.js
+++ public/js/webdashubaojs_1.js
@@ -13,6 +13,19 @@ window.onload = function() {
 
 
 };
+function web_topad()
+{
+  document.writeln("<script async src=\'//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js\'></script>");
+  document.writeln("<!-- 大书包_970X90_ad1 -->");
+  document.writeln("<ins class=\'adsbygoogle\'");
+  document.writeln("     style=\'display:inline-block;width:970px;height:90px\'");
+  document.writeln("     data-ad-client=\'ca-pub-1047803567334338\'");
+  document.writeln("     data-ad-slot=\'6528656449\'></ins>");
+  document.writeln("<script>");
+  document.writeln("(adsbygoogle = window.adsbygoogle || []).push({});");
+  document.writeln("</script>");  
+}
+
 
 $(document).ready(function()
 {
--- resources/views/novel/layouts/default.blade.php
+++ resources/views/novel/layouts/default.blade.php
@@ -85,6 +85,7 @@
   </div>
 
   <div class="index_ad">
+    <script>web_topad();</script>
   </div>
   @yield('content')
   <div class="index_ad">
