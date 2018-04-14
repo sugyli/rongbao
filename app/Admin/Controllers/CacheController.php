df20e351e9ed1779c3a7b3ee2f0ad0ed919f5740
“liunxsugyli” <3101967255@qq.com>
2018-04-14 23:12:29 +0800

    4.14.9

--- app/Admin/Controllers/CacheController.php
+++ app/Admin/Controllers/CacheController.php
@@ -67,8 +67,8 @@ class CacheController extends Controller
         $web_url = config('app.web_dashubao_url') .'/purge'.$houzui['path'];
         $curl->get($web_url);
         $curl->close();
-        $bookData = $article->getBidBookData($bid);
-        dd($bookData);
+        //$bookData = $article->getBidBookData($bid);
+
         $msg = $this->success( "4444444");
         return back()->with($msg);
 
