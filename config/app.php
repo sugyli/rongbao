2137033476db6745198a86cd6af37c802f18c17d
“liunxsugyli” <3101967255@qq.com>
2018-08-23 01:18:14 +0800

    8.23.1

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
 
     //兼容
     //'default_route'=>env('default_route' ,true),
-    'default_js'=>env('default_js','/js/webdashubaojs_27.js'),
+    'default_js'=>env('default_js','/js/webdashubaojs_28.js'),
     //'custom_key_twd'=>env('custom_key_twd',false),
     /*
     |--------------------------------------------------------------------------
